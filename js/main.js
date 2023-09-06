f60dd62ed8bf53ebea8cdfba1e5749c4b0f525d7
KevDonovan <66144711+KevDonovan@users.noreply.github.com>
2023-09-05 21:52:02 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let score = 0;
 let remainingCards = 52;
 let playerHand = ['inactive', 'inactive', 'inactive', 'inactive'];
 let houseHand = ['inactive', 'inactive', 'inactive', 'inactive'];
-let targetScore = 33;
+let targetScore = 21;
 
 document.querySelector('#openModal').addEventListener('click', openModal);
 document.querySelector('#closeModal').addEventListener('click', closeModal);
@@ -32,39 +32,47 @@ function closeModal() {
     overlay.classList.remove('active');
 }
 
-function shuffle() {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            deckID = data.deck_id;
-            drawHand();
-        })
-        .catch(err => {
-            console.log(`error ${err}`);
-    });
-}
+async function shuffle() {
+    try{
+        const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+        const data = await res.json();
 
-
-function drawHand() {
-    for(let i = 0; i < playerCards.length && i < remainingCards; i++) {
-        if(playerHand[i] === 'inactive') drawCard(i, 'player');
-        if(houseHand[i] === 'inactive') drawCard(i, 'house');
+        console.log(data);
+        deckID = data.deck_id;
+        drawHand();
+    }
+    catch (error) {
+        console.log(`error ${error}`);
     }
 }
 
-function drawCard(n, hand) {
-    fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-    .then(res => res.json())
-    .then(data => {
-        remainingCards = data.remaining;
-        document.querySelector("#remain").innerHTML = remainingCards;
+function drawHand() {
+    for(let i = 0; i < playerCards.length; i++) {
+        if(playerHand[i] === 'inactive' && remainingCards > 0) drawCard(i, 'player');
+        if(houseHand[i] === 'inactive' && remainingCards > 0) drawCard(i, 'house');
+    }
+}
+
+async function drawCard(n, hand) {
+    try{
+        const res = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+        const data = await res.json();
         if (hand === 'player') playerCards[n].src = data.cards[0].image;
         else houseCards[n].src = data.cards[0].image;
         data.cards[0].point = setPoint(data.cards[0]);
         if (hand === 'player') playerHand[n] = data.cards[0];
         else houseHand[n] = data.cards[0];
-    })
+        setRemainingCards(data.remaining);
+        console.log(remainingCards);
+    }
+    catch (error) {
+        console.log(`error ${error}`);
+    }
+}
+
+function setRemainingCards(remaining) {
+    remainingCards = remaining;
+    document.querySelector("#remain").innerHTML = remainingCards;
 }
 
 function setPoint(card) {
@@ -109,7 +117,6 @@ function housePlay() {
         houseCards[n].src = "css/assets/back.png";
         //drawCard(n, 'house');
     }
-    console.log(remainingCards);
     checkEnd();
 }
 
